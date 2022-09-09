80439b2b048d0c7fcb5f8119023c1b4cbd53c621
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-09 23:26:55 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-       char c;
-       for (c = 'z'; c >= 'a'; c--)
-	{
-         putchar(c);
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+   putchar(c);
 	
-       }
-        putchar('\n');
+     }
+     putchar('\n');
 	
-return (0);
+     return (0);
 	
 }
 
