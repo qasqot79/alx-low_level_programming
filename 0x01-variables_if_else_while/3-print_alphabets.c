0c05910f725ace4c6aff5cb7d6686f5f97e24a5d
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-09 18:45:34 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,34 +7,22 @@
 */
 int main(void)
 {
-	  char c;
+char lower_case;
+char upper_case;
 
-         for (c = 'a'; c <= 'z'; c++)
-
-	{
-
-		putchar(c);
-
-	}
-
-	for (c = 'A'; c <= 'Z'; c++)
-
-	{
-
-		putchar(c);
-	}
-
-	       putchar('\n');
-
-	       return (0);
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)	
+{
+putchar(lower_case);	
 }
 
+for  (upper_case = 'A'; upper_case <= 'Z'; upper_case++)	
+{
+putchar(upper_case);
+}	
+putchar('\n');
 
-
-
-
-
-
-
-
-
+return (0);
+	
+}
+	
+        
