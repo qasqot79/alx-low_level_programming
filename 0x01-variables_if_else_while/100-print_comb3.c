7f76603fb13c02d2d7a449fbbc60a6bb44f05fa3
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-10 15:01:08 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-              int c, i;
+                  int c, i;
                   for (c = '0'; c <= '9'; c++)
                   {
                          for (i = '0'; i <= '9'; i++)
@@ -15,14 +15,14 @@ int main(void)
                                 {
                                     putchar(c);
                                     putchar(i);
-                                    if (c != '8' || (c == '8' && i != '9'))
+                                    if (c ! = '8' || (c == '8' && i ! = '9'))
                                     {
                                           putchar(',');
                                           putchar(' ');
-                                     }
-                            }
+                                    }
+                           }
                     }
             }
             putchar('\n');
-  return (0);
+    return (0);
 }
