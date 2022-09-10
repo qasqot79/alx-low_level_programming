69464618bf155349a2fd9fea2fca0d31a96a3ae0
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-10 13:08:25 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,8 +9,9 @@ int main(void)
      char alphabet;
      for (alphabet = 122 ; alphabet >='97; alphabet--)
      {
-          putchar(alphabet;
+          putchar(alphabet);
      }
      putchar('\n');
+          
      return (0);	
 }
