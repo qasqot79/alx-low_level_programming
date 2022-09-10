c080088a4f3099a485b195b388e54a9a951cb95a
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-10 15:05:43 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 	   char c;
-	   for (c = 'a'; c <= 'z'; c++)
-	   putchar(c);
+	       for (c = 'a'; c <= 'z'; c++)
+	           putchar(c);
 	   
-	   putchar('\n');
+	           putchar('\n');
 	
 	   return (0);
 }
