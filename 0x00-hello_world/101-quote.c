fc21932106ab266972c4bdc3aaf0e6a5e6b1cfc9
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-12 20:03:02 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,4 @@
 #include <unistd.h>
-<<<<<<< HEAD
-=======
-/**
- *main-program entry point.
- *
- *Return: 1 on success.
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
-
-
->>>>>>> ba123eb1dd59d708314417fcf604e27eca6fb021
 
 /**
 * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -29,5 +14,3 @@ write(2,
 
 return (1);
 }
-
-c
