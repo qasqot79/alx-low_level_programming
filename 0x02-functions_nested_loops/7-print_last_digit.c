8f3eec024ad62f6efcad73c1a582aea79e6ffe85
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-13 19:55:22 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print the last digit of a number.
+- entry point
+ * @number: integer
+ * Description: return 1 if c is in lower case
+ * Return: 1 or 0
+ */
+int print_last_digit(int number)
+{
+int lastDigit = number % 10;
+if (lastDigit < 0)
+{
+lastDigit = lastDigit * (-1);
+_putchar('0' + lastDigit);
+return (lastDigit);
+}
+_putchar('0' + lastDigit);
+return (0);
+}
