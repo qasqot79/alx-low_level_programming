e8ef212c4e7690d7e264b0ca6bbcb97b73a2145e
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-13 19:38:52 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 char ch;
 int line;
 for (line = 0; line < 10; line++)
-	{
+{
 for (ch = 'a'; ch <= 'z'; ch++)
-		{
+{
  _putchar(ch);
-	}
+}
 _putchar('\n');
-	}
+}
 }
