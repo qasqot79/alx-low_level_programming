777ee294e32a20076d7a5e13756bad7d35aa8e6c
qasqot79 <cird2007@yahoo.com>
2022-09-13 09:23:45 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print the alphabet in lower case followed by a new line.
+*
+Return: 0
+void print_alphabet(void)
+{
+         char c;
+ 
+         for (c = 'a'; c <= 'z'; c++)
+{
+                 _putchar(c);
+_putchar('\n');
+}
