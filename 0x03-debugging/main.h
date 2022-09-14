fdbddc0b61a72a8dcc22ed5108443f8b2deb07c2
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-14 19:14:11 +0100

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -7,4 +7,5 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month; int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
+
 #endif
