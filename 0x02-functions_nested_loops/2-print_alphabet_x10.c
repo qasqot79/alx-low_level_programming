76793a05c0d808fba35835369dea395dffa1b5d9
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-14 17:10:38 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
- */
+ * print_alphabet - prints the alphabet in lowercase,followed by a new line.
+ * Return: Alawys 0
+*/
 void print_alphabet(void)
 {
 char letter;
