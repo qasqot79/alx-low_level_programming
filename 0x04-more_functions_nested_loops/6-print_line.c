3ee45c50e943c04a8e81177dfbf905fd4ccaa65f
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-15 14:24:50 +0100

    task 0

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - function outputs _____
+ *
+ *@n: is int for line
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int ns;
+
+	for (ns = 0; ns < n; ns++)
+	{
+		if (n <= 0)
+		{
+			_putchar ('\n');
+		}
+		_putchar (95);
+	}
+			 _putchar ('\n');
+}
