dd0afb4d3548c642ddc942593603abc9516d07b5
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-15 13:55:11 +0100

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,14 @@
  *
  * Return: 0 or 1
  */
-
-
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
+	{	
+	return (1);
+	}	
+       else
+	{	
+       return (0);
+        }   
 }
