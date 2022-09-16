3cb214a45ee8486501424393d65a5c128bc7874c
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-16 21:17:40 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 
 int _isupper(int c);
 int _isdigit(int c);
