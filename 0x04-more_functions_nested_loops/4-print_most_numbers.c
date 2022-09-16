6e985553335d44dd940f8fb9e381c47d640e4178
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-16 16:31:20 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,11 @@ int n;
 
 for (n = 48; n < 58; n++)
 {
-if (n == 50) ́|| (n == 52))
+if ((n == 50) ́|| (n == 52))
 {
-continue
+continue;
 }
-_putchar(n + '0');
+_putchar(n);
 }
 _putchar(10);
 }
