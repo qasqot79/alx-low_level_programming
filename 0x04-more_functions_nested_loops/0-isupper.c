c4e232adf11b348ec59835df6645be13b1da8bf2
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-16 17:07:07 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 /**
  * _isupper - int _isupper(int c) function returns 1 if c is uppercase
@@ -9,7 +8,6 @@
  */
 int _isupper(int c)
 {
-<<<<<<< HEAD
 if (c >= 'A' && c <= 'Z')
 {	
 return (1);
@@ -19,10 +17,8 @@ else
 return (0);
 }
 } 
-=======
 	if (c > 64 && c < 91)
 		return (1);
 	else
 		return (0);
-}
->>>>>>> e92630ab46429fe51a04583f90c9e0a2e715fb3b
+} 
