3c0c8f618ab4ea281f08a8f47344dc60bd53bc6a
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-19 13:44:51 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - Returns the length of a string
-* @str: The string to get the length of.
+ * @str: The string to get the length of.
  * Return: The length of @str.
  */
 size_t _strlen(const char *str)
