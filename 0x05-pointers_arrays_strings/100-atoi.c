26abd38b6ff1893ac90c4f88e139a8cb0f303df7
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-19 16:40:21 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,8 @@ int _atoi(char *s)
 {
 int sign = 1;
 unsigned int num = 0;
-do {if (*s == '-')
+do if (*s == '-')
+{
 sign *= -1;        
 else if (*s >= '0' && *s <= '9')
 num = (num * 10) + (*s - '0');
