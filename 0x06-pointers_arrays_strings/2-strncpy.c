23ec6b291a18fb9b670cd8017a01ea086cf55a44
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-21 14:43:23 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,24 +2,22 @@
 
 /**
 * main - function that copies a string. 
-  * works exactly like strncpy.
- * 
+* works exactly like strncpy.
+* 
 * Return:  Pointer resulting to a string.
- */
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-        int index = 0, src_len = 0;
-        while (src[index++])
-
-                 src_len++;
-        for (index = 0; src[index] && index < n; index++)
+         int index = 0, src_len = 0;
+       
+         while (src[index++])
+                  src_len++;
+        
+         for (index = 0; src[index] && index < n; index++)
                                  dest[index] = src[index];
 
-        for (index = src_len; index < n; index++)
-                 dest[index] = '\0';
-
-
-
+         for (index = src_len; index < n; index++)
+                             dest[index] = '\0';
 
          return (dest);
 } 
