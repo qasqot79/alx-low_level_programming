b9be5a61d0c18e42ef4605ed89da0dd646e724d2
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-21 18:25:58 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,13 @@
  */
 char *strcat(char *dest, const char *src)
 {
-        int dest_len = _strlen(dest);
-	int i;
-	int n = _strlen(src);
+	int index = 0, dest_len = 0;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
