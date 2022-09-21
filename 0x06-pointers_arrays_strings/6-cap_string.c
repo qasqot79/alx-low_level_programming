1add2c9b50a989e15e64cc1e73e2ba0b801b6013
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-21 16:25:47 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,18 +15,18 @@ char *cap_string(char *)
 			index++;
 
 		if (str[index - 1] == ' ' ||
-		   str[index - 1] == '\t' ||
-                   str[index - 1] == '\n' ||
-                  str[index - 1] == ',' ||
-                  str[index - 1] == ';' ||
+		    str[index - 1] == '\t' ||
+                    str[index - 1] == '\n' ||
+                    str[index - 1] == ',' ||
+                    str[index - 1] == ';' ||
                     str[index - 1] == '.' ||
                     str[index - 1] == '!' ||
-                     str[index - 1] == '?' ||
+                    str[index - 1] == '?' ||
                     str[index - 1] == '"' ||
-                   str[index - 1] == '(' ||
+                    str[index - 1] == '(' ||
                     str[index - 1] == ')' ||
                     str[index - 1] == '{' ||
-                     str[index - 1] == '}' ||
+                    str[index - 1] == '}' ||
                         index == 0)
                       str[index] -= 32;
          index++;
