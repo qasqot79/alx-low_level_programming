aa12e32daa3cabf31f7a612e2b1263a3c4ba472b
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-21 21:10:09 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * strcat - Concatenates the string pointed to by @src,
  * terminate null byte, to the end of the string pointed to by @dest.
@@ -11,12 +12,9 @@
 char *strcat(char *dest, const char *src)
 {
 int index = 0, dest_len = 0;
-
 while (dest[index++])
 dest_len++;
-
 for (index = 0; src[index]; index++)
 dest[dest_len++] = src[index];
-
 return (dest);
 }
