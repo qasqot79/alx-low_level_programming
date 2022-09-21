fcf6596ad032f3a216376fc2a157bf16440dba08
qasqot79 <cird2007@yahoo.com>
2022-09-21 04:38:50 -0700

    task 0-2

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function that concatenates two strings.
+ *
+ * Return: Always 0.
+*/
+char *_strcat(char *dest, char *src)
+{
+         int index = 0, dest_len = 0;
+         
+        while (dest[index++])
+                dest_len++;
+        
+         for (index = 0; src[index]; index++)
+                             dest[dest_len++] = src[index];
+        
+         return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**  
+ * _strncat - function that concatenates two strings,does not need to 
+ * be null-terminated if it contains n or more bytes.
+  * Return:  Pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+       int index = 0, dest_len = 0;
+        
+       while (dest[index++])
+                dest_len++;
+       for (index = 0; src[index] && index < n; index++)
+                            dest[dest_len++] = src[index];
+       
+       return (dest);
+}
