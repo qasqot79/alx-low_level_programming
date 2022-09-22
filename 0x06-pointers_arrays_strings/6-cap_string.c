f4e84f7a59ae24fb28a14f7a8581265f9886c297
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-22 12:55:37 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include<stdio.h>
+
 /**
  * *cap_string - function capitalizes all words of a string
  *
@@ -7,7 +8,6 @@
  *
  * Return: char *
  */
-
 char *cap_string(char *n)
 {
 	int count;
