924b3f43f4a3009cb83bc9b4d45a09db0d007da6
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-22 12:52:41 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,42 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words in a string
- * @s: string to capitalize
+ * cap_string - capitalice all words in a string
+ * @str: string
  *
- * Return: address of s
+ * Return: string
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
+	int i = 0;
 
-	while (*(s + i))
+	if (str[0] >= 97 && str[i] <= 122)
+		str[0] = (str[0] - 32);
+
+	while (str[i])
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		if ((str[i] == ',') || (str[i] == ';') ||
+		(str[i] == '!') || (str[i] == '.') ||
+		(str[i] == '?') || (str[i] == '"') ||
+		(str[i] == '(') || (str[i] == ')') ||
+		(str[i] == '{') || (str[i] == '}') ||
+		(str[i] == '\n') || (str[i] == ' '))
 		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
-			{
-				for (j = 0; j <= 12; j++)
-				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
-				}
-			}
+			i++;
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = (str[i] - 32);
+			i--;
+		}
+
+		else if (str[i] == '\t')
+		{
+			str[i] = (str[i] + 23);
+			i++;
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = (str[i] - 32);
+			i--;
 		}
 		i++;
 	}
-	return (s);
+	return (str);
 }
