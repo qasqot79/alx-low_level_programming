11cb17109246c0ecbea0889db69a79278cae7fcf
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-26 18:12:24 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	unsigned int i;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
 
-	return (memory);
+	return (s);
 }
