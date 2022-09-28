43f1e0c5998756225aac925442a896a315215f12
qasqot79 <111513209+qasqot79@users.noreply.github.com>
2022-09-28 16:15:52 +0100

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,7 @@ char *_strcat(char *dest, char *src)
        int i, c;
 
        for (i = 0; dest[i] != '\0'; i++)
-
        	        ;
-
        for (c = 0; src[c] != '\0'; c++)
        {
                dest[i] = src[c];
